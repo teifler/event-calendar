6d70d5f4a3ec118eba8529f5fee918e843af077b
Tim Eifler <tim.eif@protonmail.com>
2022-04-07 12:52:52 +0200

    Add styling and functionality to show dates in calendar

--- src/App.js
+++ src/App.js
@@ -63,7 +63,100 @@ function App() {
     thirdPlasticTrash,
   ];
 
+  const onChange = date => {
+    setDate(date);
+  };
 
+  function isSameDay(eventDates, date) {
+    return differenceInCalendarDays(eventDates, date) === 0;
+  }
+
+  function tileClassName({ date, view }) {
+    const output = [];
+    if (
+      view === 'month' &&
+      recylingDates.find(recylingDate => isSameDay(recylingDate, date))
+    ) {
+      output.push('positionRel');
+    }
+
+    if (
+      view === 'month' &&
+      paperTrashDates.find(eventDate => isSameDay(eventDate, date))
+    ) {
+      output.push('positionRel');
+    }
+
+    if (
+      view === 'month' &&
+      plasticTrashDates.find(eventDate => isSameDay(eventDate, date))
+    ) {
+      output.push('positionRel');
+    }
+
+    return output;
+  }
+
+  function tileContent({ date, view }) {
+    //Check each entry of eventDates against date to see if it should be highlighted
+
+    const output = [];
+    if (
+      view === 'month' &&
+      recylingDates.find(recylingDate => isSameDay(recylingDate, date))
+    ) {
+      output.push(
+        <svg
+          className="recyling-icon"
+          width="6"
+          height="6"
+          viewBox="0 0 10 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="10" height="10" fill="#ff5c8a" />
+        </svg>
+      );
+    }
+
+    if (
+      view === 'month' &&
+      paperTrashDates.find(eventDate => isSameDay(eventDate, date))
+    ) {
+      console.log(date);
+      output.push(
+        <svg
+          className="papertrash-icon"
+          width="6"
+          height="6"
+          viewBox="0 0 10 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="5" cy="5" r="5" fill="#06d6a0" />
+        </svg>
+      );
+    }
+
+    if (
+      view === 'month' &&
+      plasticTrashDates.find(eventDate => isSameDay(eventDate, date))
+    ) {
+      output.push(
+        <svg
+          className="plastictrash-icon"
+          width="7"
+          height="7"
+          viewBox="0 0 13 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M6.5 0L12.9952 11.25H0.00480938L6.5 0Z" fill="#ee9b00" />
+        </svg>
+      );
+    }
+
+    return output;
   }
 
   return (
@@ -74,6 +167,45 @@ function App() {
         tileClassName={tileClassName}
         value={date}
       />
+      <div className="container">
+        <h3>Legende: </h3>
+        <div className="infoBox">
+          <p>Recyling</p>
+          <svg
+            width="10"
+            height="10"
+            viewBox="0 0 10 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="10" height="10" fill="#ff5c8a" />
+          </svg>
+        </div>
+        <div className="infoBox">
+          <p>Plastictrash</p>
+          <svg
+            width="10"
+            height="10"
+            viewBox="0 0 13 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M6.5 0L12.9952 11.25H0.00480938L6.5 0Z" fill="#ee9b00" />
+          </svg>
+        </div>
+        <div className="infoBox">
+          <p>Papertrash</p>
+          <svg
+            width="10"
+            height="10"
+            viewBox="0 0 10 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="5" cy="5" r="5" fill="#06d6a0" />
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
